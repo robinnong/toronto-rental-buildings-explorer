a72b8c147a7117e6a14e127c3dd01cbb7d86f255
Robin Nong <robin@thinkdataworks.com>
2025-08-12 17:18:29 -0400

    Adding no results found message

--- src/app/search/components/SearchResultsList.tsx
+++ src/app/search/components/SearchResultsList.tsx
@@ -17,7 +17,7 @@ export default function SearchResultsList({
     useContext(SearchContext);
 
   return (
-    <div className="flex flex-col gap-2 w-full mb-4">
+    <div className="flex flex-col gap-2 h-full w-full mb-4">
       {filteredSearchResults?.length > 0 && (
         <div className="flex justify-between">
           <span>
@@ -60,8 +60,19 @@ export default function SearchResultsList({
       </ul>
 
       {!isLoading && filteredSearchResults?.length === 0 && (
-        <div className="text-center py-4">
-          <p>No results found</p>
+        <div className="flex flex-col items-center justify-center h-full gap-2">
+          <i className="fa-solid fa-magnifying-glass fa-2x mb-2 text-gray-400" />
+          <h3 className="text-lg font-bold">No results found</h3>
+          <p>Sorry, we couldn't find any results matching your search.</p>
+          <button
+            type="button"
+            className="text-cyan-700 mt-2"
+            onClick={() => {
+              // TODO
+            }}
+          >
+            Clear filters
+          </button>
         </div>
       )}
 
