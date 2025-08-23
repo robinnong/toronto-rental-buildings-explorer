d6ec2ae443b14bb8f884b6e8f1cca6ec237f156c
Robin Nong <robin@thinkdataworks.com>
2025-08-23 16:37:56 -0400

    Updating next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Required for static export to GitHub Pages
+  basePath: isProd ? "/toronto-rental-buildings-explorer" : "",
+  assetPrefix: isProd ? "/toronto-rental-buildings-explorer" : "",
 };
 
 export default nextConfig;
