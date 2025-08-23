cefc0794bd128b0c7d4966af6ded6f228c00e3c6
Robin Nong <robin@thinkdataworks.com>
2025-08-23 16:52:57 -0400

    Updating next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // Required for static export to GitHub Pages
