c8628c176da10f491b8f2487fb92b20b5bbd234f
Robin Nong <robin@thinkdataworks.com>
2025-08-23 16:35:26 -0400

    Clear next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Required for static export to GitHub Pages
-  basePath: isProd ? "/toronto-rental-buildings-explorer" : "",
-  assetPrefix: isProd ? "/toronto-rental-buildings-explorer" : "",
 };
 
 export default nextConfig;
