ba32705779d90ab4dd519ecaafbc9f9dc025a021
Robin Nong <robin@thinkdataworks.com>
2025-08-23 16:02:26 -0400

    Updating next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // env: {
-  //   GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
-  //   FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  //   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-  //   FIREBASE_AUTH_DOMAIN: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  //   FIREBASE_STORAGE_BUCKET: `${process.env.FIREBASE_PROJECT_ID}.firebasestorage.app`,
-  //   FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  //   FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-  //   ALGOLIA_API_KEY: process.env.ALGOLIA_API_KEY,
-  //   ALGOLIA_APPLICATION_ID: process.env.ALGOLIA_APPLICATION_ID,
-  //   ALGOLIA_INDEX_NAME: process.env.ALGOLIA_INDEX_NAME,
-  // },
+  output: "export", // Required for static export to GitHub Pages
+  basePath: "/toronto-rental-buildings-explorer/",
+  assetPrefix: "/toronto-rental-buildings-explorer/",
 };
 
 export default nextConfig;
