89b6e6453421e964495dacc35aa3c106409bf0ec
Robin Nong <robin@thinkdataworks.com>
2025-08-31 21:16:45 -0400

    Refactoring for readability

--- src/app/lib/generateSearchClauses.ts
+++ src/app/lib/generateSearchClauses.ts
@@ -3,10 +3,8 @@ import searchQueryBuilder from "./searchQueryBuilder";
 
 // Generates Algolia-compatible search clauses from a list of applied building features filters
 // Example output: BARRIER_FREE_ACCESSIBILTY_ENTR:true AND CONFIRMED_STOREYS: 5 TO 14'
-const generateSearchClauses = (
-  currentBuildingFeatureFilters: FilterType[]
-): string => {
-  const clauses = currentBuildingFeatureFilters.reduce((acc, curr) => {
+const generateSearchClauses = (filters: FilterType[]): string => {
+  const clauses = filters.reduce((acc, curr) => {
     acc.push(searchQueryBuilder(curr));
     return acc;
   }, [] as string[]);
